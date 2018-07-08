f9a50f1a00c9f8dd0c1870a9a534fec9cc64cba6
Prateek <prateek.ashtikar512@gmail.com>
2018-07-09 03:35:14 +0530

    Added details of Spring JDBCTemplate

--- spring-batch-classic-db/src/main/java/com/prateek/listeners/OrdersStepListeners.java
+++ spring-batch-classic-db/src/main/java/com/prateek/listeners/OrdersStepListeners.java
@@ -1,20 +1,38 @@
 package com.prateek.listeners;
 
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import com.prateek.mapper.OrdersRowMapper;
+import com.prateek.model.Orders;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
 public class OrdersStepListeners implements StepExecutionListener{
+	@Autowired
+	private DataSource dataSource;
+	
 	
 	@Override
 	public void beforeStep(StepExecution stepExecution) {
 		log.info("pipe step listener called");
-	    stepExecution.getExecutionContext().putString("customerNumber", "363");
+		
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		
+		List<String> results = jdbcTemplate.queryForList("SELECT customerNumber FROM orders  ORDER BY customerNumber desc LIMIT 1", String.class);
+		if(!results.isEmpty()) {
+			stepExecution.getExecutionContext().putString("customerNumber", results.get(0));
+		}
 	}
 
 	@Override
